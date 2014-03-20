f3c56a6ca98fa50173f4d35bf2705991a9fd1ec1
Siwen Yu <yusiwen@gmail.com>
2014-03-20 14:30:49 +0800

    Delete ec.py.* and consolidate
    Former-commit-id: c40775e140e2fc11daf1f105b025d1462628ac3a

--- eclipse/ec.py
+++ eclipse/ec.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import os
 import pygtk
 pygtk.require('2.0')
 import gtk
@@ -7,51 +7,50 @@ import subprocess
 
 class EclipseChooser:
     i = 1
-    commands = ["ec-java-indigo.sh","ec-java-kepler.sh","ec-jee-indigo.sh","ec-rcp-indigo.sh"]
+    eclipse_dir = "/opt/eclipse"
+
+    commands = []
+
+    def find_eclipses(self, path):
+        names = os.listdir(path)
+        for name in names:
+            tmp_path = os.path.join(path, name)
+            if os.path.isdir(tmp_path):
+                temp_list = []
+                temp_list.append(name)
+                temp_list.append(os.path.join(tmp_path, "eclipse"))
+                self.commands.append(temp_list)
 
     def callback(self, widget, data):
         self.i = data
 
     def __init__(self):
+        self.find_eclipses(self.eclipse_dir)
+
         dialog = gtk.Dialog("Eclipse Environment Chooser", None, 0, (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OK, gtk.RESPONSE_OK))
         dialog.set_default_size(250, 300)
         label = gtk.Label("Choose:")
-        
+
         dialog.vbox.pack_start(label, True, True, 0)
 
-        button1 = gtk.RadioButton(None, "JDT Indigo SR2")
-        button1.connect("toggled", self.callback, 1)
-        #button.set_active(True)
-        dialog.vbox.pack_start(button1, True, True, 0)
-        #button.show()
+        button = None
+        offset = 1
+        for x in self.commands:
+            temp_button = gtk.RadioButton(button, x[0])
+            temp_button.connect("toggled", self.callback, offset)
+            dialog.vbox.pack_start(temp_button, True, True, 0)
+            button = temp_button
+            offset = offset + 1
 
-        button2 = gtk.RadioButton(button1, "JDT Kepler SR1")
-        button2.connect("toggled", self.callback, 2)
-        dialog.vbox.pack_start(button2, True, True, 0)
-        #button.show()
-
-        button3 = gtk.RadioButton(button1, "Java EE Indigo SR2")
-        button3.connect("toggled", self.callback, 3)
-        dialog.vbox.pack_start(button3, True, True, 0)
-        #button.show()
-
-        button4 = gtk.RadioButton(button1, "RCP/PDE Indigo SR2")
-        button4.connect("toggled", self.callback, 4)
-        dialog.vbox.pack_start(button4, True, True, 0)
-        #button.show()
-        
         dialog.show_all()
 
         response = dialog.run()
         
         if response == gtk.RESPONSE_OK:
-            cmd = "/home/yusiwen/" + self.commands[self.i-1]
-            #print(self.i)
-            #print(cmd)
+            cmd = self.commands[self.i-1][1]
             subprocess.Popen([cmd])
         
         dialog.destroy()
-      
 
 if __name__ == "__main__":
 	EclipseChooser()
