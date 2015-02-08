fd5d10f021e7b994b9127ab878f93c3f32402c23
Siwen Yu <yusiwen@gmail.com>
2015-02-08 18:35:06 +0800

    Update ec.py
    Former-commit-id: e39365a162eec0fde8f8ab796c5ac813de9ab79e

--- eclipse/ec.py
+++ eclipse/ec.py
@@ -8,27 +8,45 @@ import subprocess
 class EclipseChooser:
     i = 1
     eclipse_dir = "/opt/eclipse"
-    eclipse_args = ['eclipse', '-vm', '/opt/java/jdk1.7.0_65/jre/bin/java', '-nosplash']
+    eclipse_args = ['eclipse', '-vm', '/opt/java/jdk1.7.0_72/jre/bin/java', '-nosplash']
+    idea_dir = '/opt/intellij'
+    idea_args = ['bin/idea.sh']
 
     commands = []
+    
+    def check_env(self):
+        # Linux is the default os
+        # Windows ?
+        if os.name == 'nt':
+            self.eclipse_dir = 'j:/eclipse'
+            self.eclipse_args[0] = 'eclipse.exe'
+            self.eclipse_args[2] = 'j:/java/jdk1.7.0_65/jre/bin/javaw.exe'
+            self.idea_dir = 'j:/intellij'
+            self.idea_args = ['bin/idea.exe']
 
-    def find_eclipses(self, path):
-        names = os.listdir(path)
-        for name in names:
-            tmp_path = os.path.join(path, name)
-            if os.path.isdir(tmp_path):
-                temp_list = []
-                temp_list.append(name)
-                temp_args_list = list(self.eclipse_args)
-                temp_args_list[0] = os.path.join(tmp_path, temp_args_list[0])
-                temp_list.append(temp_args_list)
-                self.commands.append(temp_list)
+    def find(self, path, args):
+        if os.path.exists(path):
+            names = os.listdir(path)
+            for name in names:
+                tmp_path = os.path.join(path, name)
+                if os.path.isdir(tmp_path):
+                    temp_list = []
+                    temp_list.append(name)
+                    temp_args_list = list(args)
+                    temp_args_list[0] = os.path.join(tmp_path, temp_args_list[0])
+                    temp_list.append(temp_args_list)
+                    # print(temp_args_list)
+                    self.commands.append(temp_list)
 
     def callback(self, widget, data):
         self.i = data
 
     def __init__(self):
-        self.find_eclipses(self.eclipse_dir)
+        
+        self.check_env()
+        
+        self.find(self.eclipse_dir, self.eclipse_args)
+        self.find(self.idea_dir, self.idea_args)
 
         dialog = gtk.Dialog("Eclipse Environment Chooser", None, 0, (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OK, gtk.RESPONSE_OK))
         dialog.set_default_size(250, 300)
@@ -56,4 +74,4 @@ class EclipseChooser:
         dialog.destroy()
 
 if __name__ == "__main__":
-	EclipseChooser()
+    EclipseChooser()
