95a1f7c2e01f910f9abaff8e1cac574d37238841
Siwen Yu <yusiwen@gmail.com>
2015-05-30 16:55:15 +0800

    Remove mutt QQMail configs
    Former-commit-id: 6232586d514d8789df1f48087fd5e94734be5b02

--- mutt/mutt_get_qq_password.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import keyring
-
-passwd = keyring.get_password("mutt","siwen.yu@foxmail.com")
-print 'set imap_pass = "%s"' % (passwd,)
-print 'set smtp_pass = "%s"' % (passwd,)
