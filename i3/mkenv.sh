08a2531e0f2e389b025d01af374165a2c6fc8e31
Siwen Yu <yusiwen@gmail.com>
2015-11-27 22:16:03 +0800

    Update scripts for mac & virtual ubuntu machine on mac
    Former-commit-id: 9f11fe8a9037a6410fffd4b990a53147ec50b48b

--- i3/mkenv.sh
+++ i3/mkenv.sh
@@ -14,4 +14,17 @@ ln -sf $CONFIG_HOME/dunst/dunstrc $DUNST_HOME/dunstrc
 ln -sf $CONFIG_HOME/i3bang/i3bang.rb $HOME/bin/i3bang
 i3bang
 
+# check if 'consolekit' is installed or not
+PACKAGE=$(dpkg -l | grep consolekit)
+if [ -z "$PACKAGE" ]; then
+  # Install 'consolekit'
+  echo 'Install consolekit...'
+  sudo apt-get install consolekit
+  if [ "$?" -ne 0  ]; then
+    echo 'Install consolekit failed, please check the output of apt-get.'
+    exit 1
+  fi
+  echo 'Install consolekit ... done'
+fi
+
 sudo cp $CONFIG_HOME/xsessions/i3.desktop /usr/share/xsessions/i3.desktop
