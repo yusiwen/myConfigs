1d1f736f47383e716740e6d0fc131bda13fbb15e
Siwen Yu <yusiwen@gmail.com>
2016-02-02 22:28:39 +0800

    Update shell/mkenv.sh
    Add installation of ZSH if it does not exist.
    
    
    Former-commit-id: 9bc8708a111e7ce95b1031cfe345079b13627e23

--- shell/mkenv.sh
+++ shell/mkenv.sh
@@ -2,6 +2,22 @@
 
 CONFIG_SHELL=$HOME/myConfigs/shell
 
+if [ ! "$SHELL" = "/usr/bin/zsh" ]; then
+  echo "Current SHELL is not ZSH"
+  if [ ! -e /usr/bin/zsh ]; then
+    echo "Cannot find ZSH binary, installing ..."
+    sudo apt-get install zsh
+    if [ "$?" -ne 0 ]; then
+      echo "Install ZSH failed, please manually install it."
+      exit
+    fi
+    echo "Install ZSH ... Done."
+    echo "Change SHELL to /usr/bin/zsh ..."
+    chsh -s /usr/bin/zsh
+    echo "Change SHELL to /usr/bin/zsh ... Done."
+  fi
+fi
+
 ln -sfnv $CONFIG_SHELL/bashrc $HOME/.bashrc
 ln -sfnv $CONFIG_SHELL/bash_aliases $HOME/.bash_aliases
 ln -sfnv $CONFIG_SHELL/bash_profile $HOME/.bash_profile
