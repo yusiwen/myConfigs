890156f4abca9b938b8cd4db16e971ea063b6cd8
Siwen Yu <yusiwen@gmail.com>
2016-02-12 07:39:20 -0800

    Add rxvt-unicode-256color installation script
    Former-commit-id: 5e463d33ca945318715534c54370d89e3359ce03

--- X11/mkenv.sh
+++ X11/mkenv.sh
@@ -5,3 +5,17 @@ ln -sfnv $HOME/myConfigs/X11/Xresources $HOME/.Xresources
 ln -sfnv $HOME/myConfigs/X11/themes/jellybeans.xresources $HOME/.Xresources.theme
 ln -sfnv $HOME/myConfigs/X11/fonts/input-mono-compressed.xresources $HOME/.Xresources.font
 xrdb -load $HOME/.Xresources
+
+PROXY=""
+if [ "$1" = "-p"  ]; then
+  if [ -e $HOME/.apt.conf  ]; then
+    PROXY="-c $HOME/.apt.conf"
+    echo "$PROXY is set"
+  fi
+fi
+
+RXVT_PACAKGE=$(dpkg -l|cut -d " " -f 3|grep "rxvt-unicode-256color")
+if [ -z "$RXVT_PACAKGE" ]; then
+  echo "Installing rxvt-unicode-256color..."
+  sudo apt-get $PROXY install rxvt-unicode-256color
+fi
