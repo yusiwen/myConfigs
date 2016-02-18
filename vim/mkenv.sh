a89ba04755a80e21b97c1e8e3382d7bdb0187ffe
Siwen Yu <yusiwen@gmail.com>
2016-02-18 07:07:56 -0800

    Update vim/mkenv.sh for OS specifications
    Former-commit-id: 53091ea2859897f676f7686b9ef73c721fc1d067

--- vim/mkenv.sh
+++ vim/mkenv.sh
@@ -5,17 +5,31 @@
 CONFIG_VIM=$HOME/myConfigs/vim
 VIM_HOME=$HOME/.vim
 
-# Check if vim-gtk is installed or not
-PACKAGE=$(dpkg -l | grep vim-gtk)
-if [ -z "$PACKAGE" ]; then
-  # Install vim-gtk
-  echo 'Install vim-gtk ...'
-  sudo apt-get $APT_PROXY install vim-gtk
-  if [ "$?" -ne 0 ]; then
-    echo 'Install vim-gtk failed, please check the output of apt-get.'
-    exit 1
+if [ $(uname) = 'Linux' ]; then
+  if [ $(lsb_release -i -s) = 'Ubuntu' ]; then
+    echo 'Ubuntu is found, checking vim-gtk...'
+    # Check if vim-gtk is installed or not
+    PACKAGE=$(dpkg -l | grep vim-gtk)
+    if [ -z "$PACKAGE" ]; then
+      echo 'vim-gtk is not found.'
+      # Install vim-gtk
+      echo 'Install vim-gtk ...'
+      sudo apt-get $APT_PROXY install vim-gtk
+      if [ "$?" -ne 0 ]; then
+        echo 'Install vim-gtk failed, please check the output of apt-get.'
+        exit 1
+      fi
+      echo 'Install vim-gtk ... done'
+    fi
+    echo 'vim-gtk is found.'
   fi
-  echo 'Install vim-gtk ... done'
+elif [ $(uname) = 'Darwin' ]; then
+  PACKAGE=$(brew list|grep vim)
+  if [ -z "$PACKAGE" ]; then
+    brew install vim macvim
+  fi
+else
+  echo 'Unknown OS, please make sure vim is installed.'
 fi
 
 ln -sfnv $CONFIG_VIM/vimrc $HOME/.vimrc
@@ -44,4 +58,8 @@ if [ ! -d "$VIM_HOME/bundle/neobundle.vim" ]; then
   echo 'Install vim plugins ...'
   ~/.vim/bundle/neobundle.vim/bin/neoinstall
   echo 'Install vim plugins ... done'
+  if [ -d $VIM_HOME/bundle/vimproc.vim ]; then
+    cd $VIM_HOME/bundle/vimproc.vim && make
+    cd $OLDPWD
+  fi
 fi
