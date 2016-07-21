50509eaf7d52a05d6e9542439b84bce6241f706b
Siwen Yu <yusiwen@gmail.com>
2016-07-21 08:01:01 -0700

    Update install_tools.sh
    Former-commit-id: 14c8c88fa4c27ce8d650b9f31a70aa78a85207b9

--- install_tools.sh
+++ install_tools.sh
@@ -2,6 +2,5 @@
 
 sudo apt-get install curl tsocks silversearcher-ag ruby exuberant-ctags fcitx fcitx-sunpinyin astyle figlet toilet toilet-fonts
 
-$HOME/myConfigs/exuberant-ctags/mkenv.sh
 $HOME/myConfigs/figlet/mkenv.sh
 
