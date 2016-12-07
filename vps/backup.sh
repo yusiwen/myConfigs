9a551f9dce15a07b020fc1c0da91771f68e19c8c
Siwen Yu <yusiwen@gmail.com>
2016-12-06 23:48:38 -0800

    Update vps/bakcup.sh
    Former-commit-id: 205c45fe45acdd4719ffcc3e49bff9f5ff30e99d

--- vps/backup.sh
+++ vps/backup.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 # Backup MySql database backup files from vps01
+echo 'Backup mysql databases...'
 rsync -azPv root@vps01:/var/vmail/backup/mysql $HOME/vps/backup/mysql
 
 # Backup let's encrypt certificates from vps01
-rsync -azPv root@vps01:/etc/letsencrypt $HOME/vps/backup/etc/letsencrypt
+echo "Backup let's encrypt certificates..."
+rsync -azPv root@vps01:/etc/letsencrypt $HOME/vps/backup/etc
 
 # Backup Jenkins backup files from vps01
+echo 'Backup Jenkins jobs...'
 rsync -azPv root@vps01:/var/lib/jenkins/backup $HOME/vps/backup/jenkins
+
+# Backup Nginx setting files from vps01
+echo 'Backup Nginx settings...'
+rsync -azPv root@vps01:/etc/nginx $HOME/vps/backup/etc
 
