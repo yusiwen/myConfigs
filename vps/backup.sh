17fe116a69a29890ece326627426eda34d682b8e
Siwen Yu <yusiwen@gmail.com>
2016-12-12 04:21:45 -0800

    Update vps/backup.sh
    Add backup for Artifactory backups
    
    
    Former-commit-id: 28cd92fdca38bac577c7dcc90399bda388defbe3

--- vps/backup.sh
+++ vps/backup.sh
@@ -16,3 +16,6 @@ rsync -azPv root@vps01:/var/lib/jenkins/backup $HOME/vps/backup/jenkins
 echo 'Backup Nginx settings...'
 rsync -azPv root@vps01:/etc/nginx $HOME/vps/backup/etc
 
+# Backup Artifactory backup files from vps01
+echo 'Backup Artifactory files...'
+rsync -azPv root@vps01:/opt/artifactory/artifactory-oss-4.14.2/backup $HOME/vps/backup/artifactory
