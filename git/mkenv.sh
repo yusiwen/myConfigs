331a57f97cacc440882892d53d46b5796b87ecbb
Siwen Yu <yusiwen@gmail.com>
2017-01-02 14:17:36 +0800

    Update git/mkenv.sh
    Make more compatible for Windows/Linux/Darwin
    
    
    Former-commit-id: 81c22e733bac21bf5bab58e4d72bb630a428fe1c

--- git/mkenv.sh
+++ git/mkenv.sh
@@ -1,21 +1,38 @@
 #!/bin/sh
 
-. $HOME/myConfigs/gfw/get_apt_proxy.sh
+if [ -d "$HOME/myConfigs" ]; then
+  . $HOME/myConfigs/gfw/get_apt_proxy.sh
+fi
 
-# install git if not exist
-GIT_SOURCE="^deb http://ppa.launchpad.net/git-core/ppa/ubuntu $(lsb_release -c -s) main"
-APT_SOURCE=$(grep "$GIT_SOURCE" /etc/apt/sources.list.d/*.list)
-if [ -z "$APT_SOURCE" ]; then
-  echo "Add git-core ppa..."
-  sudo apt-add-repository ppa:git-core/ppa
-  sudo apt-get $APT_PROXY update
-  sudo apt-get $APT_PROXY install git
+OS=$(uname)
+echo "$OS found..."
+if [ $OS = 'Linux' ]; then
+  # install git if not exist
+  GIT_SOURCE="^deb http://ppa.launchpad.net/git-core/ppa/ubuntu $(lsb_release -c -s) main"
+  APT_SOURCE=$(grep "$GIT_SOURCE" /etc/apt/sources.list.d/*.list)
+  if [ -z "$APT_SOURCE" ]; then
+    echo "Add git-core ppa..."
+    sudo apt-add-repository ppa:git-core/ppa
+    sudo apt-get $APT_PROXY update
+  fi
+
+  if [ -z $(dpkg -l | awk '{print $2}' | grep -e '^git$') ]; then
+    sudo apt-get $APT_PROXY install git
+  fi
 fi
 
 git config --global user.email "yusiwen@gmail.com"
 git config --global user.name "Siwen Yu"
 
-git config --global core.autocrlf input
+if [ ${OS:0:5} = 'MINGW' ]; then
+  # On Windows, commit with LF and checkout with CRLF
+  git config --global core.autocrlf true
+else
+  # On Linux or Mac, commit with LF and no change on checkout
+  git config --global core.autocrlf input
+fi
+# Turn on warning on convert EOL failure
+git config --global core.safecrlf warn
 
 git config --global core.editor "vim"
 git config --global merge.tool vimdiff
@@ -25,9 +42,11 @@ git config --global mergetool.prompt false
 git config --global http.proxy 'http://d.qypac.net:15355'
 git config --global https.proxy 'http://d.qypac.net:15355'
 
+if [ $OS = 'Linux' ] || [ $OS = 'Darwin' ]; then
 mkdir -p $HOME/.ssh
 ln -sfnv $HOME/myConfigs/git/ssh_config $HOME/.ssh/config
 
 mkdir -p $HOME/bin
 ln -sfnv $HOME/myConfigs/git/git-migrate $HOME/bin/git-migrate
 ln -sfnv $HOME/myConfigs/git/git-new-workdir $HOME/bin/git-new-workdir
+fi
