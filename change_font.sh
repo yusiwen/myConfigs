64724e478e99b764d296ac12a50221cc2e48d2bd
Siwen Yu <yusiwen@gmail.com>
2017-09-22 21:47:44 -0700

    Add Fira Code font to X11 Xresources
    Former-commit-id: 7da2dd11499bbd084a142c9db1ab14f57b57496d

--- change_font.sh
+++ change_font.sh
@@ -6,6 +6,7 @@ X11_FONT_FILE="$HOME/.Xresources.font"
 echo "[1] Input Mono Compressed"
 echo "[2] Iosevka"
 echo "[3] Iosevka Slab"
+echo "[4] Fira Code"
 echo -n "Choose font[1]: "
 read number
 
@@ -22,6 +23,9 @@ elif echo "$number" | grep -iq "^2"; then
 elif echo "$number" | grep -iq "^3"; then
   echo "Setting font to 'Iosevka Slab'..."
   ln -sfnv $X11_FONT_DIR/iosevka-slab.xresources $X11_FONT_FILE
+elif echo "$number" | grep -iq "^4"; then
+  echo "Setting font to 'Fira Code'..."
+  ln -sfnv $X11_FONT_DIR/firacode.xresources $X11_FONT_FILE
 else
   echo "Nahh!"
   exit
