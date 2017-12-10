3d759f562e32941d1b5cde461001155222ec19d9
Siwen Yu <yusiwen@gmail.com>
2017-12-10 14:14:20 +0800

    Update scripts for make node.js env
    Former-commit-id: bd5803ecaadf5fbb4df69ff3094979691ac4ecd1

--- node.js/mkenv.sh
+++ node.js/mkenv.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-. $HOME/myConfigs/gfw/get_apt_proxy.sh
+. $HOME/myConfigs/gfw/get_apt_proxy.sh $1
 
 NODE_PACKAGE=$(dpkg -l|cut -d " " -f 3|grep "nodejs")
 if [ -z "$NODE_PACKAGE" ]; then
 
   echo "[1] Node.js v4"
-  echo "[1] Node.js v5"
-  echo "[1] Node.js v6"
-  echo -n "Choose version[1]:"
+  echo "[2] Node.js v6"
+  echo "[3] Node.js v8"
+  echo -n "Choose version[3]:"
   read version
 
   if [ -z $version ]; then
-    version='1'
+    version='3'
   fi
 
   if echo "$version" | grep -iq "^1"; then
     curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
   elif echo "$version" | grep -iq "^2"; then
-    curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-  elif echo "$version" | grep -iq "^3"; then
     curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+  elif echo "$version" | grep -iq "^3"; then
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
   else
     echo "Nahh!"
     exit
@@ -36,9 +36,5 @@ if [ -z "$NODE_PACKAGE" ]; then
 fi
 
 mkdir -p $HOME/.npm-packages
-
-ln -sfnv $HOME/myConfigs/node.js/npmrc $HOME/.npmrc
-
-echo "Installing cnpm..."
-npm install -g cnpm --registry=https://registry.npm.taobao.org
+cp $HOME/myConfigs/node.js/npmrc $HOME/.npmrc
 
