a1b1d697a983572f5ab53254b0cc49aecd237c2e
Siwen Yu <yusiwen@gmail.com>
2017-12-23 00:22:16 +0800

    Update vps/backup.sh
    Former-commit-id: d407eb68790fa4b1408f879a4fe8e1be57c3ecfa

--- vps/backup.sh
+++ vps/backup.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
+TARGET_PATH=
+if [ -z $1 ]; then
+  TARGET_PATH=$HOME/vps/backup
+else
+  TARGET_PATH=$1
+  if [ ! -d $TARGET_PATH ]; then
+    echo "$TARGET_PATH is not a valid path!"
+    exit
+  fi
+fi
+
 # Backup MySql database backup files from vps01
 echo 'Backup mysql databases...'
-rsync -azPv root@vps01:/var/vmail/backup/mysql $HOME/vps/backup/mysql
+rsync -azPv root@vps01:/var/vmail/backup/mysql $TARGET_PATH
 
 # Backup let's encrypt certificates from vps01
 echo "Backup let's encrypt certificates..."
-rsync -azPv root@vps01:/etc/letsencrypt $HOME/vps/backup/etc
+rsync -azPv root@vps01:/etc/letsencrypt $TARGET_PATH/etc
 
 # Backup Jenkins backup files from vps01
 echo 'Backup Jenkins jobs...'
-rsync -azPv root@vps01:/var/lib/jenkins/backup $HOME/vps/backup/jenkins
+rsync -azPv root@vps01:/var/lib/jenkins/backup $TARGET_PATH/jenkins
 
 # Backup Nginx setting files from vps01
 echo 'Backup Nginx settings...'
-rsync -azPv root@vps01:/etc/nginx $HOME/vps/backup/etc
+rsync -azPv root@vps01:/etc/nginx $TARGET_PATH/etc
 
 # Backup Artifactory backup files from vps01
 echo 'Backup Artifactory files...'
-rsync -azPv root@vps01:/var/opt/jfrog/artifactory/backup $HOME/vps/backup/artifactory
+rsync -azPv root@vps01:/var/opt/jfrog/artifactory/backup $TARGET_PATH/artifactory
