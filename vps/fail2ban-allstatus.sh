354561331433cdd8af34e5dc330988019a977b01
Siwen Yu <yusiwen@gmail.com>
2019-08-16 12:13:28 +0800

    Ling shell script

--- vps/fail2ban-allstatus.sh
+++ vps/fail2ban-allstatus.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-JAILS=`fail2ban-client status | grep "Jail list" | sed -E 's/^[^:]+:[ \t]+//' | sed 's/,//g'`
-for JAIL in $JAILS
-do
-  fail2ban-client status $JAIL
+JAILS=$(fail2ban-client status | grep "Jail list" | sed -E 's/^[^:]+:[ \t]+//' | sed 's/,//g')
+for JAIL in $JAILS; do
+  fail2ban-client status "$JAIL"
 done
