331df6e915c11a8843df1541fb9c00e43c40977f
Siwen Yu <yusiwen@gmail.com>
2019-09-22 20:19:03 +0800

    Adjust vim installation scripts

--- install.sh
+++ install.sh
@@ -617,41 +617,9 @@ function install_vim() { # {{{
     return
   fi
 
-  ln -sfnv "$CONFIG_VIM"/vimrc "$VIM_HOME"/vimrc
-  ln -sfnv "$CONFIG_VIM"/autoload "$VIM_HOME"/autoload
-  ln -sfnv "$CONFIG_VIM"/ftplugin "$VIM_HOME"/ftplugin
-  ln -sfnv "$CONFIG_VIM"/plugin "$VIM_HOME"/plugin
-  ln -sfnv "$CONFIG_VIM"/plugins.yaml "$VIM_HOME"/plugins.yaml
-  ln -sfnv "$CONFIG_VIM"/vimrc.defx "$VIM_HOME"/vimrc.defx
-  ln -sfnv "$CONFIG_VIM"/vimrc.denite "$VIM_HOME"/vimrc.denite
-  ln -sfnv "$CONFIG_VIM"/vimrc.deoplete "$VIM_HOME"/vimrc.deoplete
-  ln -sfnv "$CONFIG_VIM"/vimrc.filetype "$VIM_HOME"/vimrc.filetype
-  ln -sfnv "$CONFIG_VIM"/vimrc.general "$VIM_HOME"/vimrc.general
-  ln -sfnv "$CONFIG_VIM"/vimrc.mappings "$VIM_HOME"/vimrc.mappings
-  ln -sfnv "$CONFIG_VIM"/vimrc.misc "$VIM_HOME"/vimrc.misc
-  ln -sfnv "$CONFIG_VIM"/vimrc.neomake "$VIM_HOME"/vimrc.neomake
-  ln -sfnv "$CONFIG_VIM"/vimrc.goyo "$VIM_HOME"/vimrc.goyo
-  ln -sfnv "$CONFIG_VIM"/vimrc.theme "$VIM_HOME"/vimrc.theme
-  ln -sfnv "$CONFIG_VIM"/ctags "$HOME"/.ctags
-  ln -sfnv "$CONFIG_VIM"/spell "$VIM_HOME"/spell
-
-  #Default theme
-  ln -sfnv "$CONFIG_VIM"/themes/vimrc.theme.sourcerer "$HOME"/.vim/vimrc.colortheme
-
-  # link custom color themes to $VIM_HOME
-  if [ ! -L "$VIM_HOME"/colors ]; then
-    ln -sfnv "$CONFIG_VIM"/colors "$VIM_HOME"/colors
-  fi
-
-  # link snippets to $VIM_HOME
-  if [ ! -L "$VIM_HOME"/snippets ]; then
-    ln -sfnv "$CONFIG_VIM"/snippets "$VIM_HOME"/snippets
-  fi
-
-  ln -sfnv "$CONFIG_VIM"/init.vim "$VIM_HOME"/init.vim
-  ln -sfnv "$CONFIG_VIM"/vimrc.neovim "$VIM_HOME"/vimrc.neovim
+  ln -sfnv "$CONFIG_VIM" "$VIM_HOME"
   mkdir -p "$HOME"/.config
-  ln -sfnv "$HOME"/.vim "$HOME"/.config/nvim
+  ln -sfnv "$CONFIG_VIM" "$HOME"/.config/nvim
 
   # Initialize Python 2 & 3 environment for NeoVim
   VARPATH="$HOME"/.cache/vim
