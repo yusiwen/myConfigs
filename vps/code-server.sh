f5113bb43d40ac4ec2009db40e1d6c13cadd9ff0
Siwen Yu <yusiwen@gmail.com>
2020-05-04 12:35:00 +0800

    update code-server docker startup script

--- vps/code-server.sh
+++ vps/code-server.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 docker run -d -p 127.0.0.1:3030:8080 \
-       -v "/var/lib/coder:/home/coder/.local/share/code-server" \
-       -v "/var/lib/coder-project:/home/coder/project" \
-       -e "PASSWORD=XXXXXX" \
-       codercom/code-server:3.1.0
+  --name code \
+  --restart unless-stopped \
+  -v "/var/lib/coder:/home/coder" \
+  -v "/var/lib/coder-project:/home/coder/project" \
+  -v "/opt/jdk1.8.0_192:/home/coder/jdk1.8.0_192" \
+  -v "/opt/apache-maven-3.6.1:/home/coder/apache-maven-3.6.1" \
+  -e "PASSWORD=XXX" \
+  codercom/code-server:latest
 
