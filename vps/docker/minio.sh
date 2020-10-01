eb1e2817a30a9676eef21d9db4f875420fca4d06
Siwen Yu <yusiwen@gmail.com>
2020-10-01 13:03:44 +0800

    remove key exposure in script

--- vps/docker/minio.sh
+++ vps/docker/minio.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+ACCESSKEY=$(head -1 /root/.minio.pwd)
+SECRETKEY=$(tail -1 /root/.minio.pwd)
+
 docker run -d -p 127.0.0.1:9000:9000 \
   --name minio \
   -v /data/share:/data \
-  -e "MINIO_ACCESS_KEY=B97Q9ZiDWMv4az7RcGfD" \
-  -e "MINIO_SECRET_KEY=cQoKNKFNaxibj/6NUVLhM/Lnf2kCGuajoNmRyxDj" \
+  -e "MINIO_ACCESS_KEY=$ACCESSKEY" \
+  -e "MINIO_SECRET_KEY=$SECRETKEY" \
   minio/minio server /data
 
