7f3414083770e9c8090605fcc993ac590babadc6
Siwen Yu <yusiwen@gmail.com>
2020-10-05 19:34:43 +0800

    add all-in-one clash container startup script

---
+++ gfw/clash-all-in-one.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run -d \
+  --name clash-client \
+  --restart unless-stopped \
+  -p 7890:7890 \
+  -p 7891:7891 \
+  -p 7892:7892 \
+  -p 9092:9090 \
+  -v ~/tmp/clash/config.yaml:/root/.config/clash/config.yaml \
+  harbor.yusiwen.cn/library/clash-client:latest
+
