b0206d470ac926e52a14ac85de4b220c6f0af71a
Siwen Yu <yusiwen@gmail.com>
2020-10-19 01:22:28 +0000

    add file input support

--- docker/sync-google-containers.sh
+++ docker/sync-google-containers.sh
@@ -14,44 +14,62 @@ OS=$(uname)
 OS_ARCH=$(uname -m)
 echo -e "${COLOR}Operate System: ${COLOR1}$OS-$OS_ARCH${COLOR} found...${NC}"
 
+function sync() {
+  SITE_NAME=$(echo -n "$1" | cut -d '/' -f1)
+  PACKAGE_NAME="$(echo -n ${1#*/} | cut -d ':' -f1)"
+  VERSION_NAME=$(echo -n "$1" | cut -d ':' -f2)
+  ARCH_NAME='amd64'
+  if [ "$OS_ARCH" = 'aarch64' ]; then
+    ARCH_NAME='arm64'
+  fi
+  
+  TARGET_SITE='harbor.yusiwen.cn'
+  LIBRARY_NAME=$SITE_NAME
+  if [ "$SITE_NAME" = 'k8s.gcr.io' ]; then
+    LIBRARY_NAME='google_containers'
+  fi
+  
+  echo "SITE_NAME=$SITE_NAME"
+  echo "LIBRARY_NAME=$LIBRARY_NAME"
+  echo "PACKAGE_NAME=$PACKAGE_NAME"
+  echo "VERSION_NAME=$VERSION_NAME"
+  echo "ARCH_NAME=$ARCH_NAME"
+  
+  NEW_TAG=$TARGET_SITE/$LIBRARY_NAME/$PACKAGE_NAME-$ARCH_NAME:$VERSION_NAME
+  echo "NEW_TAG=$NEW_TAG"
+  
+  echo -e "${COLOR}Pull image: ${COLOR1}$1${COLOR} ...${NC}"
+  docker pull $1
+  
+  echo -e "${COLOR}Tag image: ${COLOR1}$NEW_TAG${COLOR} ...${NC}"
+  docker tag  $1 $NEW_TAG
+  
+  echo -e "${COLOR}Push image: ${COLOR1}$NEW_TAG${COLOR} ...${NC}"
+  docker push $NEW_TAG
+}
+
+function sync_from_file() {
+  input="$1"
+  while IFS= read -r line; do
+    sync "$line"
+  done < "$input"
+}
+
 if [ -z "$1" ]; then
   echo 'sync.sh REPO:VERSION'
   exit 1
+elif [ "$1" = '-f' ]; then
+  if [ -z "$2" ]; then
+    echo 'sync.sh -f FILENAME'
+    exit 1
+  fi
+
+  sync_from_file $2
+else
+  if ! echo -n "$1" | grep -q ':'; then
+    echo 'sync.sh REPO:VERSION'
+    exit 1
+  else
+    sync $1
+  fi
 fi
-
-if ! echo -n "$1" | grep -q ':'; then
-  echo 'sync.sh REPO:VERSION'
-  exit 1
-fi
-
-SITE_NAME=$(echo -n "$1" | cut -d '/' -f1)
-PACKAGE_NAME="$(echo -n ${1#*/} | cut -d ':' -f1)"
-VERSION_NAME=$(echo -n "$1" | cut -d ':' -f2)
-ARCH_NAME='amd64'
-if [ "$OS_ARCH" = 'aarch64' ]; then
-  ARCH_NAME='arm64'
-fi
-
-TARGET_SITE='harbor.yusiwen.cn'
-LIBRARY_NAME=$SITE_NAME
-if [ "$SITE_NAME" = 'k8s.gcr.io' ]; then
-  LIBRARY_NAME='google_containers'
-fi
-
-echo "SITE_NAME=$SITE_NAME"
-echo "LIBRARY_NAME=$LIBRARY_NAME"
-echo "PACKAGE_NAME=$PACKAGE_NAME"
-echo "VERSION_NAME=$VERSION_NAME"
-echo "ARCH_NAME=$ARCH_NAME"
-
-NEW_TAG=$TARGET_SITE/$LIBRARY_NAME/$PACKAGE_NAME-$ARCH_NAME:$VERSION_NAME
-echo "NEW_TAG=$NEW_TAG"
-
-echo -e "${COLOR}Pull image: ${COLOR1}$1${COLOR} ...${NC}"
-docker pull $1
-
-echo -e "${COLOR}Tag image: ${COLOR1}$NEW_TAG${COLOR} ...${NC}"
-docker tag  $1 $NEW_TAG
-
-echo -e "${COLOR}Push image: ${COLOR1}$NEW_TAG${COLOR} ...${NC}"
-docker push $NEW_TAG
