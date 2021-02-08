cb95a67f5feef21625478f5aaa1b89c53fb6e29a
Siwen Yu <yusiwen@gmail.com>
2021-02-08 15:59:52 +0800

    update code-server.sh

--- vps/docker/code-server.sh
+++ vps/docker/code-server.sh
@@ -2,11 +2,11 @@
 
 docker run -d \
   -p 127.0.0.1:3030:8080 \
-  --name code-3.7.4 \
+  --name code \
   --restart unless-stopped \
   -v "/var/lib/coder:/home/coder" \
   -v "/var/lib/coder-project:/home/coder/project" \
   -v "/opt/jdk1.8.0_192:/home/coder/jdk1.8.0_192" \
   -v "/opt/apache-maven-3.6.1:/home/coder/apache-maven-3.6.1" \
-  codercom/code-server:3.7.4
+  codercom/code-server:3.8.1
 
