36ba8ccb08d7ba62b8865c19d80da4b6616815e4
Siwen Yu <yusiwen@gmail.com>
2021-03-22 10:45:27 +0800

    update docker container startup scripts

--- vps/docker/code-server.sh
+++ vps/docker/code-server.sh
@@ -8,11 +8,12 @@ docker run -d \
   --env GOPROXY=https://goproxy.io,direct \
   --env GOPATH=/var/lib/go_packages \
   --env JAVA_HOME=/usr/local/jdk-11.0.10 \
+  --env M2_HOME=/usr/local/apache-maven-3.6.3 \
   -v "/var/lib/coder:/home/coder" \
   -v "/var/lib/coder-project:/home/coder/project" \
   -v "/opt/jdk-11.0.10:/usr/local/jdk-11.0.10" \
   -v "/opt/go:/usr/local/bin/go" \
   -v "/opt/go_pkg:/var/lib/go_packages" \
-  -v "/opt/apache-maven-3.6.1:/home/coder/apache-maven-3.6.1" \
+  -v "/opt/apache-maven-3.6.3:/usr/local/apache-maven-3.6.3" \
   codercom/code-server:latest
 
