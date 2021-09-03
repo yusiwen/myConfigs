e0640afe2bc380d43b66642b1eefb87d66aac80c
Siwen Yu <yusiwen@gmail.com>
2021-09-03 20:36:41 +0800

    update minio.sh

--- vps/docker/minio.sh
+++ vps/docker/minio.sh
@@ -3,11 +3,11 @@
 ACCESSKEY=$(awk -F "=" '/MINIO_AK/ {print $2}' /root/.my.pwd.cnf)
 SECRETKEY=$(awk -F "=" '/MINIO_SK/ {print $2}' /root/.my.pwd.cnf)
 
-docker run -d -p 127.0.0.1:9000:9000 \
+docker run -d \
+  -p 127.0.0.1:9000:9000 \
   --name minio \
   --restart unless-stopped \
   -v /data/share:/data \
   -e "MINIO_ACCESS_KEY=$ACCESSKEY" \
   -e "MINIO_SECRET_KEY=$SECRETKEY" \
-  minio/minio:latest server /data
-
+  bitnami/minio:latest
