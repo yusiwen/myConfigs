696eadf3fde6ebb3c662f0729f9283602094ba19
Siwen Yu <yusiwen@gmail.com>
2021-10-04 15:25:14 +0800

    update ss-v2ray.sh

--- vps/docker/ss-v2ray.sh
+++ vps/docker/ss-v2ray.sh
@@ -5,6 +5,6 @@ SSPWD=$(awk -F "=" '/SS_PWD/ {print $2}' /root/.my.pwd.cnf)
 docker run -d \
   --name ss-v2ray \
   --restart unless-stopped \
-  -p 127.0.0.1:1080:1080 \
+  --network host \
   -e PASSWD="$SSPWD" \
-  harbor.yusiwen.cn/library/ss-v2ray:v3.3.5
+  harbor.yusiwen.cn/library/ss-v2ray:v3.3.5-slim
