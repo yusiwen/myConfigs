7959a667b38c48b5ac89b851c6c994ac043da04f
Siwen Yu <yusiwen@gmail.com>
2021-10-09 10:36:58 +0800

    update drone.sh

--- vps/docker/drone.sh
+++ vps/docker/drone.sh
@@ -16,14 +16,13 @@ docker run -d \
   -e DRONE_RPC_SECRET=${DRONE_RPC_SECRET} \
   -e DRONE_RUNNER_CAPACITY=2 \
   -e DRONE_RUNNER_NAME=${HOSTNAME} \
-  drone/drone-runner-docker:1
+  drone/drone-runner-docker:latest
 
 docker run -d \
   --name drone \
   --restart unless-stopped \
   -p 127.0.0.1:8900:80 \
-  -v /var/lib/drone:/data/docker/drone \
-  -e DRONE_AGENTS_ENABLED=true \
+  -v /var/lib/drone:/data \
   -e DRONE_GITEA_SERVER=${DRONE_GITEA_SERVER} \
   -e DRONE_GITEA_CLIENT_ID=${DRONE_GITEA_CLIENT_ID} \
   -e DRONE_GITEA_CLIENT_SECRET=${DRONE_GITEA_CLIENT_SECRET} \
@@ -32,4 +31,7 @@ docker run -d \
   -e DRONE_SERVER_PROTO=${DRONE_SERVER_PROTO} \
   -e DRONE_USER_CREATE=username:yusiwen,admin:true \
   -e DRONE_USER_FILTER=yusiwen \
-  drone/drone:2
+  -e DRONE_LOGS_DEBUG=true \
+  -e DRONE_LOGS_TEXT=true \
+  -e DRONE_LOGS_PRETTY=true \
+  drone/drone:latest
