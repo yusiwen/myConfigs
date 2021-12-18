8753d0a49550b4994ae2225153a76bdc28649fba
Siwen Yu <yusiwen@gmail.com>
2021-12-19 01:28:35 +0800

    update code-server.sh

--- vps/docker/code-server.sh
+++ vps/docker/code-server.sh
@@ -4,16 +4,19 @@ docker run -d \
   --network host \
   --name code \
   --restart unless-stopped \
+  --env GO111MODULE=on \
   --env GOROOT=/usr/local/bin/go \
-  --env GOPROXY=https://goproxy.io,direct \
+  --env GOPROXY=https://goproxy.cn,direct \
   --env GOPATH=/var/lib/go_packages \
-  --env JAVA_HOME=/usr/local/jdk-11.0.10 \
-  --env M2_HOME=/usr/local/apache-maven-3.6.3 \
+  --env JAVA_HOME=/usr/local/jdk17 \
+  --env M2_HOME=/usr/local/apache-maven-3.8.4 \
+  --env PATH=$GOROOT/bin:$JAVA_HOME/bin:M2_HOME/bin:$PATH \
   -v "/var/lib/coder:/home/coder" \
-  -v "/var/lib/coder-project:/home/coder/project" \
-  -v "/opt/jdk-11.0.10:/usr/local/jdk-11.0.10" \
+  -v "/opt/jdk17:/usr/local/jdk17" \
+  -v "/opt/jdk11:/usr/local/jdk11" \
+  -v "/opt/jdk8:/usr/local/jdk8" \
   -v "/opt/go:/usr/local/bin/go" \
   -v "/opt/go_pkg:/var/lib/go_packages" \
-  -v "/opt/apache-maven-3.6.3:/usr/local/apache-maven-3.6.3" \
+  -v "/opt/apache-maven-3.8.4:/usr/local/apache-maven-3.8.4" \
   codercom/code-server:latest --bind-addr 0.0.0.0:3030
 
