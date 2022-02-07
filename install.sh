0c7a78b8579f6cb2bd8d92541cd981e84aff4ecb
Siwen Yu <yusiwen@gmail.com>
2022-02-07 10:59:12 +0800

    change node.js installation logic

--- install.sh
+++ install.sh
@@ -570,7 +570,9 @@ function install_node() { # {{{
     fetch_myConfigs
   fi
 
-  if ! type nvm >/dev/null 2>&1; then
+  NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+
+  if [ ! -s "$NVM_DIR/nvm.sh" ]; then
     if [ "$OS" = 'Windows_NT' ]; then
       echo -e "${COLOR}Please download nvm-windows, and install it manually"
     else
@@ -581,8 +583,8 @@ function install_node() { # {{{
       fi
 
       curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-      nvm install node
-      nvm use node
+
+      echo -e "Please re-login and use 'nvm' to install node.js instance"
     fi
   else
     echo -e "${COLOR1}Node.js($(node -v))${COLOR} was found.${NC}"
