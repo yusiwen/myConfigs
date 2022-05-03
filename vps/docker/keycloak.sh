ced5445cd02272e61545ac9e9a4bcbb9984099a4
Siwen Yu <yusiwen@gmail.com>
2022-05-03 20:08:00 +0800

    use docker-compose to run keycloak

--- vps/docker/keycloak.sh
+++ vps/docker/keycloak.sh
@@ -1,19 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-DBPWD=$(awk -F "=" '/POSTGRES_KEYCLOAK/ {print $2}' /root/.my.pwd.cnf)
-ADMINPWD=$(awk -F "=" '/KEYCLOAK_ADMIN_PASSWORD/ {print $2}' /root/.my.pwd.cnf)
+export DBPWD=$(awk -F "=" '/POSTGRES_KEYCLOAK/ {print $2}' /root/.my.pwd.cnf)
+export ADMINPWD=$(awk -F "=" '/KEYCLOAK_ADMIN_PASSWORD/ {print $2}' /root/.my.pwd.cnf)
 
-docker run -d --name keycloak \
-  --restart unless-stopped \
-  --network postgres \
-  -p 127.0.0.1:8090:8080 \
-  -e DB_VENDOR=POSTGRES\
-  -e DB_ADDR=postgres \
-  -e DB_DATABASE=keycloak \
-  -e DB_USER=keycloak \
-  -e DB_PASSWORD="$DBPWD" \
-  -e KEYCLOAK_USER=admin \
-  -e KEYCLOAK_PASSWORD="$ADMINPWD" \
-  -e KEYCLOAK_FRONTEND_URL="https://sso.yusiwen.cn/auth" \
-  -e PROXY_ADDRESS_FORWARDING=true \
-  quay.io/keycloak/keycloak:latest
+cd ~/myDocker/keycloak
+docker-compose "$@"
