c32d5ab586879300e7f8ce671f466892e140e9f6
Siwen Yu <yusiwen@gmail.com>
2022-05-03 22:29:49 +0800

    use docker-compose to manage postgresql

--- vps/docker/postgresql.sh
+++ vps/docker/postgresql.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-ROOTPWD=$(awk -F "=" '/POSTGRES_ROOT/ {print $2}' /root/.my.pwd.cnf)
+export ROOTPWD=$(awk -F "=" '/POSTGRES_ROOT/ {print $2}' /root/.my.pwd.cnf)
 
 if [ -z "$(docker network ls | grep postgres)" ]; then
   docker network create -d bridge postgres
 fi
 
-docker run -d \
-  --name postgres \
-  --network postgres \
-  --restart unless-stopped \
-  -p 127.0.0.1:5432:5432 \
-  -e POSTGRES_PASSWORD=$ROOTPWD \
-  -v /var/lib/postgresql/data:/var/lib/postgresql/data \
-  postgres:12.8
+cd ~/myDocker/postgresql/aliyun02
+docker-compose -p postgresql "$@"
