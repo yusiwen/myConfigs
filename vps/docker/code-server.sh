83ceb7b86aa3b082cf4ce10f744e63d613b15a13
Siwen Yu <yusiwen@gmail.com>
2022-05-03 19:38:44 +0800

    use docker-compose to run code-server

--- vps/docker/code-server.sh
+++ vps/docker/code-server.sh
@@ -1,22 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker run -d \
-  --network host \
-  --name code \
-  --restart unless-stopped \
-  --env GO111MODULE=on \
-  --env GOROOT=/usr/local/bin/go \
-  --env GOPROXY=https://goproxy.cn,direct \
-  --env GOPATH=/var/lib/go_packages \
-  --env JAVA_HOME=/usr/local/jdk17 \
-  --env M2_HOME=/usr/local/apache-maven-3.8.4 \
-  --env PATH=$GOROOT/bin:$JAVA_HOME/bin:M2_HOME/bin:$PATH \
-  -v "/var/lib/coder:/home/coder" \
-  -v "/opt/jdk17:/usr/local/jdk17" \
-  -v "/opt/jdk11:/usr/local/jdk11" \
-  -v "/opt/jdk8:/usr/local/jdk8" \
-  -v "/opt/go:/usr/local/bin/go" \
-  -v "/opt/go_pkg:/var/lib/go_packages" \
-  -v "/opt/apache-maven-3.8.4:/usr/local/apache-maven-3.8.4" \
-  codercom/code-server:latest --bind-addr 0.0.0.0:3030
-
+cd ~/myDocker/code-server
+docker-compose "$@"
