6121808add60bf30f7ec69a33e08e9acd88fba4e
Siwen Yu <yusiwen@gmail.com>
2022-05-03 19:38:13 +0800

    move version to docker-compose env files

--- vps/docker/upsource.sh
+++ vps/docker/upsource.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-export UPSOURCE_VERSION=2020.1.1960
 cd ~/myDocker/upsource/aliyun01
 docker-compose up -d
--- vps/docker/wiki-docker.sh
+++ vps/docker/wiki-docker.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-export WIKIJS_VERSION=2.5.280
 cd ~/myDocker/wiki.js
 docker-compose "$@"
