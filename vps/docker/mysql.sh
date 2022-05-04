156ce31fe04514d9d5504f1e985604ba78da6617
Siwen Yu <yusiwen@gmail.com>
2022-05-04 13:08:59 +0800

    use docker-compose to run mysql

--- vps/docker/mysql.sh
+++ vps/docker/mysql.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-docker run -d -p 3307:3306 \
-  --name mysql \
-  --restart unless-stopped \
-  -v /var/lib/mysql-data:/var/lib/mysql \
-  -v /var/lib/mysql-conf:/etc/mysql/mysql.conf.d \
-  -e MYSQL_ROOT_PASSWORD=u92XGFuk5v8VCp5J \
-  mysql:5.7
+export MYSQL_ROOTPWD=$(awk -F "=" '/MYSQL_ROOTPWD/ {print $2}' /root/.my.pwd.cnf)
+
+cd ~/myDocker/mysql/aliyun01
+docker-compose -p mysql "$@"
 
