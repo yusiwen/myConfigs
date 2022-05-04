101ebc289fac8117c384fda47ad5b67ea5d2e735
Siwen Yu <yusiwen@gmail.com>
2022-05-04 13:14:31 +0800

    use docker-compose to run minio

--- vps/docker/minio.sh
+++ vps/docker/minio.sh
@@ -1,13 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-ACCESSKEY=$(awk -F "=" '/MINIO_AK/ {print $2}' /root/.my.pwd.cnf)
-SECRETKEY=$(awk -F "=" '/MINIO_SK/ {print $2}' /root/.my.pwd.cnf)
+export ACCESSKEY=$(awk -F "=" '/MINIO_AK/ {print $2}' /root/.my.pwd.cnf)
+export SECRETKEY=$(awk -F "=" '/MINIO_SK/ {print $2}' /root/.my.pwd.cnf)
 
-docker run -d \
-  -p 127.0.0.1:9000:9000 \
-  --name minio \
-  --restart unless-stopped \
-  -v /data/share:/data \
-  -e "MINIO_ACCESS_KEY=$ACCESSKEY" \
-  -e "MINIO_SECRET_KEY=$SECRETKEY" \
-  bitnami/minio:2021.6.17
+cd ~/myDocker/minio
+docker-compose "$@"
