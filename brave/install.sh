193d5e2d0b0c99529f2b1aae955fce1ac5a97066
Siwen Yu <yusiwen@gmail.com>
2022-07-04 14:33:50 +0000

    delete unused files

--- brave/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Manual install desktop application and icons
-ln -snfv $HOME/myConfigs/brave/brave.desktop $HOME/.local/share/applications/brave.desktop
-ln -snfv $HOME/myConfigs/brave/brave-web-browser.png $HOME/.local/share/icons/hicolor/48x48/apps/brave-web-browser.png
-
