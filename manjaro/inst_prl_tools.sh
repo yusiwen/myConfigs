63c376bf549983ad28658a438e23b556a709c734
Siwen Yu <yusiwen@gmail.com>
2023-02-08 10:04:34 +0800

    remove outdated manjaro settings

--- manjaro/inst_prl_tools.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo ln -sf /usr/lib/systemd/scripts/ /etc/init.d
-export def_sysconfdir=/etc/init.d
-sudo touch /etc/X11/xorg.conf
-sudo cp ./parallels-tools.service /usr/lib/systemd/system/
-sudo cp ./90-prlcc.sh /etc/X11/xinit/xinitrc.d/
-
