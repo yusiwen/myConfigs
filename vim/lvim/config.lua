8346ef31ef3648f176dd13e6d3e231b942001501
Siwen Yu <yusiwen@gmail.com>
2023-02-10 20:42:59 +0800

    update config for lunarvim

--- vim/lvim/config.lua
+++ vim/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
-lvim.colorscheme = "lunar"
+lvim.colorscheme = "nord"
 lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -165,7 +165,10 @@ lvim.builtin.treesitter.highlight.enable = true
 
 -- Additional Plugins
 lvim.plugins = {
-  {"shaunsingh/nord.nvim"},
+  { 'shaunsingh/nord.nvim' },
+  { 'tpope/vim-surround' },
+  { 'tpope/vim-repeat' },
+  { 'lambdalisue/suda.vim' },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
