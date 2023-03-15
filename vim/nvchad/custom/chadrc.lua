d945eed2cc8258b7343790cf9bb1635edc944fb7
Siwen Yu <yusiwen@gmail.com>
2023-03-15 13:42:59 +0800

    add nvchad configs

---
+++ vim/nvchad/custom/chadrc.lua
@@ -0,0 +1,26 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  transparency = true,
+
+  theme = "nord",
+  theme_toggle = { "nord", "onedark", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+
+  statusline = {
+    theme = "vscode"
+  }
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
