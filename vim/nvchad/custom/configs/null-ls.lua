1affd995525065c2ac9cac75b3497afbfb91874b
Siwen Yu <yusiwen@gmail.com>
2023-10-18 16:20:09 +0800

    add autoformat using black for python in neovim

--- vim/nvchad/custom/configs/null-ls.lua
+++ vim/nvchad/custom/configs/null-ls.lua
@@ -1,4 +1,6 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local present, null_ls = pcall(require, "null-ls")
+
 
 if not present then
   return
@@ -26,7 +28,24 @@ local sources = {
   b.formatting.black
 }
 
+local on_attach = function(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({
+      group = augroup,
+      buffer = bufnr,
+    })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+    })
+  end
+end
+
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = on_attach,
 }
