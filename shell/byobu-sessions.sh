be378385db696810645d0074a6fb71189d112aed
Siwen Yu <yusiwen@gmail.com>
2023-11-14 13:59:42 +0800

    change custom byobu launch script

--- shell/byobu-sessions.sh
+++ shell/byobu-sessions.sh
@@ -6,9 +6,9 @@ if [ -z "$session_name" ]; then
 fi
 
 # Test if there's a session already set up
-if [ -z "$(byobu list-sessions | grep $session_name)" ]; then
+if ! byobu list-sessions | grep -q "$session_name"; then
     # Create a new detached session named 'jupyter'
-    byobu new-session -d -s $session_name
+    byobu new-session -d -s "$session_name"
 fi
 # Attach to the session
-byobu attach-session -t $session_name
+byobu attach-session -t "$session_name"
