7753cc547441b1f065a4d872897ca4d20736a4bf
Siwen Yu <yusiwen@gmail.com>
2024-01-23 11:43:44 +0800

    update nvchad mappings

--- vim/nvchad/custom/mappings.lua
+++ vim/nvchad/custom/mappings.lua
@@ -4,6 +4,9 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>q"] = { ":q <CR>", "quit" },
+    ["<leader>Q"] = { ":q! <CR>", "quit without saving" },
+    ["<leader>W"] = { ":SudaWrite <CR>", "save file with sudo" },
   },
 }
 
