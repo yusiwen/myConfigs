c2b4e9872d53ce29f2f0e7df33992a96bd290493
Siwen Yu <yusiwen@gmail.com>
2024-10-12 16:28:00 +0800

    add config.fish for fish shell

---
+++ shell/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    source $HOME/myConfigs/shell/scripts/00-environment.fish
+    source $HOME/myConfigs/shell/scripts/02-functions.fish
+    source $HOME/myConfigs/shell/scripts/06-aliases.fish
+end
