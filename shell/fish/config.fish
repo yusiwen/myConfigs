f96f6e5842984ef9338f9a9f0276cd977373fa8f
Siwen Yu <yusiwen@gmail.com>
2024-10-14 13:29:11 +0800

    update fish shell configurations

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -2,5 +2,19 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source $HOME/myConfigs/shell/scripts/00-environment.fish
     source $HOME/myConfigs/shell/scripts/02-functions.fish
+    source $HOME/myConfigs/shell/scripts/03-programs.fish
     source $HOME/myConfigs/shell/scripts/06-aliases.fish
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/runtimes/miniconda3/Scripts/conda.exe
+    eval /opt/runtimes/miniconda3/Scripts/conda.exe "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/runtimes/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/runtimes/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/runtimes/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
---
+++ shell/scripts/03-programs.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+# {{{ GoLang
+if test -e /usr/local/go/bin/go || test -e /opt/go/bin/go || test -e "$HOME"/.local/go/bin/go
+  set GOROOT /usr/local/go
+  if not test -e "$GOROOT"
+    if test -e "/opt/go/bin/go"
+      set GOROOT /opt/go
+    else if test -e "$HOME"/.local/go/bin/go
+      set GOROOT "$HOME"/.local/go
+    end 
+  else
+    set GOROOT ''
+  end 
+  export GOROOT
+  export GOPATH=$HOME/.gopackages
+  mkdir -p "$GOPATH"
+  export GO111MODULE=on
+  export GOPRIVATE=git.yusiwen.cn
+  export GOPROXY=https://goproxy.cn,direct
+
+  fish_add_path "$GOROOT/bin"
+  fish_add_path "$GOPATH/bin"
+end
+#}}}
