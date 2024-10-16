e85f58fddeb38e99a4861754777b84887be939ab
Siwen Yu <yusiwen@gmail.com>
2024-10-16 16:00:54 +0800

    fix: conda initialization in fish shell

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -4,17 +4,17 @@ if status is-interactive
     source $HOME/myConfigs/shell/scripts/02-functions.fish
     source $HOME/myConfigs/shell/scripts/03-programs.fish
     source $HOME/myConfigs/shell/scripts/06-aliases.fish
-end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/runtimes/miniconda3/Scripts/conda.exe
-    eval /opt/runtimes/miniconda3/Scripts/conda.exe "shell.fish" "hook" $argv | source
-else
-    if test -f "/opt/runtimes/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/opt/runtimes/miniconda3/etc/fish/conf.d/conda.fish"
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    if test -f /opt/runtimes/miniconda3/Scripts/conda.exe
+        eval /opt/runtimes/miniconda3/Scripts/conda.exe "shell.fish" "hook" $argv | source
     else
-        set -x PATH "/opt/runtimes/miniconda3/bin" $PATH
+        if test -f "/opt/runtimes/miniconda3/etc/fish/conf.d/conda.fish"
+            . "/opt/runtimes/miniconda3/etc/fish/conf.d/conda.fish"
+        else
+            set -x PATH "/opt/runtimes/miniconda3/bin" $PATH
+        end
     end
+    # <<< conda initialize <<<
 end
-# <<< conda initialize <<<
