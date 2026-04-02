ca9aa416d72a8a6764cb3dda42b7968b6f59b377
Siwen Yu <yusiwen@gmail.com>
2026-04-02 09:15:30 +0800

    chore treesitter: simplify nvim-treesitter configuration
    - Update the require path for nvim-treesitter
    - Remove the explicit list of languages to ensure installation
    - Remove the custom configuration for highlight and indent
    
    Signed-off-by: Siwen Yu <yusiwen@gmail.com>

--- vim/nvchad/lua/configs/treesitter.lua
+++ vim/nvchad/lua/configs/treesitter.lua
@@ -1,23 +1,4 @@
-local configs = require("nvim-treesitter.config")
+local configs = require("nvim-treesitter")
 
 configs.setup({
-    ensure_installed = {
-      "bash", "c", "cpp", "go", "groovy",
-      "java", "javascript", "html", "yaml", "xml",
-      "lua", "nginx", "python", "rust",
-      "sql", "vim", "vimdoc"
-    },
-    sync_install = false,
-    highlight = {
-      enable = true,
-      disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-      end,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },  
-  })
+})
