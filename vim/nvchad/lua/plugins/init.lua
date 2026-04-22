55531f83d0395913f7bdb6e031a3f6f3dd785cc0
Siwen Yu <yusiwen@gmail.com>
2026-04-22 16:10:18 +0800

    feat(toggleterm): add toggleterm.nvim with terminal keymaps
    - Add toggleterm.nvim plugin with lazy loading configuration
    - Define keymaps for floating, horizontal, and vertical terminal toggles
    - Configure default terminal appearance and behavior options
    
    Signed-off-by: Siwen Yu <yusiwen@gmail.com>

--- vim/nvchad/lua/plugins/init.lua
+++ vim/nvchad/lua/plugins/init.lua
@@ -1,168 +1,199 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        -- event = 'BufWritePre' -- uncomment for format on save
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre' -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lus_ls", "ts_ls", "bashls", "gopls", "delve", "rust-analyzer",
+        "clangd", "pyright", "yamlls", "jsonls", "sqlls", "html",
+        "docker_language_server", "docker_compose_language_server",
+      },
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      {
+        "neovim/nvim-lspconfig",
         config = function()
-            require "configs.conform"
+          require("nvchad.configs.lspconfig").defaults()
+          require "configs.lspconfig"
         end,
+      }
     },
+  },
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-                "lus_ls", "ts_ls", "bashls", "gopls", "delve", "rust-analyzer",
-                "clangd", "pyright", "yamlls", "jsonls", "sqlls", "html",
-                "docker_language_server", "docker_compose_language_server",
-            },
-        },
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            {
-                "neovim/nvim-lspconfig",
-                config = function()
-                    require("nvchad.configs.lspconfig").defaults()
-                    require "configs.lspconfig"
-                end,
-            }
-        },
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
     },
-
-    {
-        "hedyhli/outline.nvim",
-        lazy = true,
-        cmd = { "Outline", "OutlineOpen" },
-        keys = { -- Example mapping to toggle outline
-            { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
-        },
-        opts = {
-            -- Your setup opts here
-        },
+    opts = {
+      -- Your setup opts here
     },
+  },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        --opts = overrides.treesitter,
-        branch = 'main',
-        build = ":TSUpdate",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    --opts = overrides.treesitter,
+    branch = 'main',
+    build = ":TSUpdate",
+    config = function()
+      require "configs.treesitter"
+    end,
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+      },
+      {
+        "nvim-treesitter/nvim-treesitter-context",
         config = function()
-            require "configs.treesitter"
+          require "configs.treesitter-context"
         end,
-        dependencies = {
-            {
-                "nvim-treesitter/nvim-treesitter-textobjects",
-                branch = "main",
-            },
-            {
-                "nvim-treesitter/nvim-treesitter-context",
-                config = function()
-                    require "configs.treesitter-context"
-                end,
-            },
-            {
-                "abecodes/tabout.nvim",
-                event = "InsertEnter",
-                config = function()
-                    require "configs.tabout"
-                end,
-            },
-        },
-    },
-
-    {
-        'lambdalisue/suda.vim',
-        event = { "BufNewFile", "BufReadPost" },
-    },
-
-    {
-        "tpope/vim-surround",
-        event = { "BufNewFile", "BufReadPost" },
-    },
-
-    {
-        "tpope/vim-fugitive",
-        event = { "BufNewFile", "BufReadPost" },
-    },
-
-    {
-        "RRethy/vim-illuminate",
-        event = "BufReadPost",
-        config = function()
-            require "configs.illuminate"
-        end,
-    },
-
-    {
-        "karb94/neoscroll.nvim",
-        event = "BufReadPost",
-    },
-
-    {
-        "max397574/better-escape.nvim",
+      },
+      {
+        "abecodes/tabout.nvim",
         event = "InsertEnter",
         config = function()
-            require "configs.better-escape"
+          require "configs.tabout"
         end,
+      },
     },
+  },
 
-    {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        ---@type Flash.Config
-        opts = {},
-        keys = {
-            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-        },
-    },
+  {
+    'lambdalisue/suda.vim',
+    event = { "BufNewFile", "BufReadPost" },
+  },
 
-    {
-        "mg979/vim-visual-multi",
-        event = "BufReadPost",
-        init = function()
-            require "configs.visual-multi"
-        end,
-    },
+  {
+    "tpope/vim-surround",
+    event = { "BufNewFile", "BufReadPost" },
+  },
 
-    {
-        "folke/trouble.nvim",
-        opts = {}, -- for default options, refer to the configuration section for custom setup.
-        cmd = "Trouble",
-        keys = {
-            {
-                "<leader>xx",
-                "<cmd>Trouble diagnostics toggle<cr>",
-                desc = "Diagnostics (Trouble)",
-            },
-            {
-                "<leader>xX",
-                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-                desc = "Buffer Diagnostics (Trouble)",
-            },
-            {
-                "<leader>cs",
-                "<cmd>Trouble symbols toggle focus=false<cr>",
-                desc = "Symbols (Trouble)",
-            },
-            {
-                "<leader>cl",
-                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-                desc = "LSP Definitions / references / ... (Trouble)",
-            },
-            {
-                "<leader>xL",
-                "<cmd>Trouble loclist toggle<cr>",
-                desc = "Location List (Trouble)",
-            },
-            {
-                "<leader>xQ",
-                "<cmd>Trouble qflist toggle<cr>",
-                desc = "Quickfix List (Trouble)",
-            },
-        },
+  {
+    "tpope/vim-fugitive",
+    event = { "BufNewFile", "BufReadPost" },
+  },
+
+  {
+    "RRethy/vim-illuminate",
+    event = "BufReadPost",
+    config = function()
+      require "configs.illuminate"
+    end,
+  },
+
+  {
+    "karb94/neoscroll.nvim",
+    event = "BufReadPost",
+  },
+
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require "configs.better-escape"
+    end,
+  },
+
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
     },
+  },
+
+  {
+    "mg979/vim-visual-multi",
+    event = "BufReadPost",
+    init = function()
+      require "configs.visual-multi"
+    end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    -- Trigger lazy loading on these commands
+    cmd = { "ToggleTerm", "TermExec" },
+    -- Trigger lazy loading on these keymaps
+    keys = {
+      { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm Float" },
+      { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "ToggleTerm Horizontal" },
+      { "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "ToggleTerm Vertical" },
+    },
+    opts = {
+      -- Configuration options (merged into setup)
+      size = 20,
+      open_mapping = [[<c-\>]], -- Still works as a toggle after loading
+      hide_numbers = true,
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = "float", -- Default direction
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = "curved",
+        winblend = 3,
+      },
+    },
+  },
 
 }
