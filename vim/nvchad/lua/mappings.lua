3ba64e975d77dd84afbfd4600a438544224bb489
Siwen Yu <yusiwen@gmail.com>
2026-04-29 14:16:12 +0800

    style(keymaps): refactor flash.nvim key mappings and formatting
    - Comment out the key mapping that enters command mode with the semicolon key
    - Format the flash.nvim plugin opts table to be multi-line instead of inline
    
    Signed-off-by: Siwen Yu <yusiwen@gmail.com>

--- vim/nvchad/lua/mappings.lua
+++ vim/nvchad/lua/mappings.lua
@@ -4,7 +4,7 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("n", "<leader>W", ":SudaWrite <CR>", { desc = "save file with sudo" })
